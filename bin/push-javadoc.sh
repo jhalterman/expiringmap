78d8f8d1807d7690cdd7cd478de15a750c852064
Jonathan Halterman <jhalterman@gmail.com>
2015-08-11 16:43:37 -0700

    Update javadoc script

--- bin/push-javadoc.sh
+++ bin/push-javadoc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # run from top level dir
-rm -rf docs
-git clone git@github.com:jhalterman/expiringmap.git docs -b gh-pages
+rm -rf target/docs
+git clone git@github.com:jhalterman/expiringmap.git target/docs -b gh-pages
 mvn -Pjavadoc javadoc:javadoc
-cd docs
+cd target/docs
 git add -A
 git commit -m "Updated JavaDocs"
 git push origin gh-pages
