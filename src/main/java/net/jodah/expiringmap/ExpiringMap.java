4ab50ad5bf5392bb45dc4906cc0502023df9c9f5
Jonathan Halterman <jhalterman@gmail.com>
2016-03-21 12:03:52 -0700

    Entries should hash/equals on key and value

--- src/main/java/net/jodah/expiringmap/ExpiringMap.java
+++ src/main/java/net/jodah/expiringmap/ExpiringMap.java
@@ -479,13 +479,31 @@ public class ExpiringMap<K, V> implements ConcurrentMap<K, V> {
     }
 
     @Override
-    public boolean equals(Object pOther) {
-      return key.equals(((ExpiringEntry<?, ?>) pOther).key);
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((key == null) ? 0 : key.hashCode());
+      result = prime * result + ((value == null) ? 0 : value.hashCode());
+      return result;
     }
 
     @Override
-    public int hashCode() {
-      return key.hashCode();
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      ExpiringEntry<?, ?> other = (ExpiringEntry<?, ?>) obj;
+      if (!key.equals(other.key))
+        return false;
+      if (value == null) {
+        if (other.value != null)
+          return false;
+      } else if (!value.equals(other.value))
+        return false;
+      return true;
     }
 
     @Override
